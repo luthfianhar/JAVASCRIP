c28cfa44e1478cade6510965ddbe11e3df47b074
luthfianhar <anharluthfi35@gmail.com>
2025-01-21 14:02:37 +0700

    tugas-9

--- tugas-9/script.js
+++ tugas-9/script.js
@@ -8,3 +8,20 @@ if(nilaimatematika >= 91 && nilaiipa >= 91 && nilaiips >= 91) {
     console.log('tidak lulus');
 }
 // nomor 2
+let nilaimtk =91;
+let nilaiIpa = 81;
+let nilaiIps =91;
+
+if(nilaimatematika >= 91 || nilaiipa >= 91 || nilaiips >= 91) {
+    console.log('lulus');
+}else{
+    console.log('tidak lulus');
+}
+//nomor 3
+
+let login = false;
+if (!login) {
+    console.log('anda belum login');
+}else{
+    console.log('anda sudah login');
+}
