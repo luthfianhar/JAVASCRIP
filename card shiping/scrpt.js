62fc52f5d9aa768e458a1fc5d9d4903d08b43e65
luthfianhar <anharluthfi35@gmail.com>
2025-02-25 13:46:38 +0700

    latihan-praktek

--- card shiping/scrpt.js
+++ card shiping/scrpt.js
@@ -15,3 +15,42 @@ function totalall(subtotal,shipping,discount){
 
 totalelem.innerHTML = totalall(subtotalelem,shippingelem,discountelem);
 
+function tambah(nue) {
+    let number =parseFloat(quantityelem.value);
+    number +=nue;
+    quantityelem.value = number;
+
+    let hasil=number *priceElem.innerHTML;
+    subtotalelem.innerHTML = hasil;
+
+    let total =totalall(subtotalelem,shippingelem,discountelem);
+    totalelem.innerHTML = total;
+}
+
+function kurang(nue)  {
+    let number =parseFloat(quantityelem.value);
+    number -=nue;
+    quantityelem.value = number;
+
+    let hasil=number *priceElem.innerHTML;
+    subtotalelem.innerHTML = hasil;
+
+    let total =totalall(subtotalelem,shippingelem,discountelem);
+    totalelem.innerHTML = total;
+}
+
+function btnvoucher(){
+    let inputvoucher =codevoucher.value;
+
+    if (inputvoucher == "abudzar") {
+        textvochelem.innerHTML = "voucher berhasil";
+        textvochelem.classList.remove("applied");
+        textvochelem.classList.add("success");
+        discountelem.innerHTML = 2000;
+    } else if (inputvoucher == "fulan") {
+        discountelem.innerHTML = 0;
+    }else{
+        discountelem.innerHTML = 0;
+    }
+
+}
